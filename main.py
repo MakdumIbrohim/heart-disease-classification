3e6f3c0d11a2b3f35c8d2f41ada85f3534c40d90
MakdumIbrohim <makdumibrohim3@gmail.com>
2026-01-05 16:50:28 +0700

    feat: add model evaluation with accuracy and classification report

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 data = pd.read_csv('heart.csv')
-print(data.head())
+data.head()
 
 x = data.drop(['target'], axis=1)
 y = data['target']
@@ -15,3 +15,8 @@ X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 
 model = DecisionTreeClassifier(random_state=42)
 model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print(f'Akurasi: {accuracy_score(y_test, y_pred)}\n')
+print(f'Laporan Klasifikasi:\n{classification_report(y_test, y_pred)}\n')
+print(f'Matriks Kebingungan:\n{confusion_matrix(y_test, y_pred)}')
