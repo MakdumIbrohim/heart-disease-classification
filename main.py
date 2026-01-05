e2dcf2faad390f8bb9e11060161b83f7dcdffbed
MakdumIbrohim <makdumibrohim3@gmail.com>
2026-01-05 16:18:47 +0700

    Tambahkan pelatihan model menggunakan Decision Tree

--- main.py
+++ main.py
@@ -12,3 +12,6 @@ x = data.drop(['target'], axis=1)
 y = data['target']
 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+model = DecisionTreeClassifier(random_state=42)
+model.fit(X_train, y_train)
