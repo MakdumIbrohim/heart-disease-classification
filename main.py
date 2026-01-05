526c469b36f3bdb728a340c0941a10a6932b1319
MakdumIbrohim <makdumibrohim3@gmail.com>
2026-01-05 16:12:06 +0700

    feat: add data loading and train-test split for heart dataset

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+data = pd.read_csv('heart.csv')
+print(data.head())
+
+x = data.drop(['target'], axis=1)
+y = data['target']
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
